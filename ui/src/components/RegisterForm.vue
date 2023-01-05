1afa8ed1e1c33b54c3e11c4b70951295ed7c8413
evabronskayaa <eva27122002b@gmail.com>
2023-01-05 17:57:54 +0500

    Vue: register form added

---
+++ ui/src/components/RegisterForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="form-card">
+    <h1>Sign Up</h1>
+    <form method="post">
+      <div class="form-field">
+        <img src="../assets/img/icon1.png" alt="icon1"/>
+        <input name="username" type="text" placeholder="username"/>
+      </div>
+
+      <div class="form-field">
+        <img src="../assets/img/icon2.png" alt="icon2"/>
+        <input name="password" type="password" minlength="6" placeholder="password"/>
+      </div>
+
+      <div class="form-field">
+        <img src="../assets/img/icon1.png" alt="icon3"/>
+        <input name="name" type="text" placeholder="your name"/>
+      </div>
+
+      <button type="submit">Sign Up</button>
+    </form>
+    <h3>Already have an account? <a href="#">Log In.</a></h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegisterForm"
+}
+</script>
+
+<style>
+
+</style>
